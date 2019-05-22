b5bdb8137fa6fc7d74e261890b1bd0fa81461b67
ssoso <sohi8290@naver.com>
2019-05-22 12:39:36 +0000

    190522 create

--- PROJECT03/boards/urls.py
+++ PROJECT03/boards/urls.py
@@ -19,5 +19,6 @@ from . import views
 app_name='boards'
 
 urlpatterns = [
+    path('new/', views.new, name='new'),
     path('', views.index, name='index'),
 ]
--- PROJECT03/boards/views.py
+++ PROJECT03/boards/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
 
 # Create your views here.
 def index(request):
     boards = Board.objects.all()
     return render(request, 'boards/index.html', {'boards' : boards})
+    
+def new(request):
+    if request.method == 'POST':
+        # create 수행
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        board = Board(title=title, content=content)
+        board.save()
+        
+        return redirect('boards:index')
+    else:
+        # new 페이지 보여줌
+        return render(request, 'boards/new.html')
